3f2220b8fc20d4dcb2c0554b03b5ac45566ed48d
Dave <dave.w.mckinnon@gmail.com>
2022-06-07 18:54:54 -0700

    fix api call function

--- src/App.jsx
+++ src/App.jsx
@@ -3,33 +3,10 @@ import React from "react";
 import Form from "./components/Form";
 
 function App() {
-  const axios = require("axios");
-  let result;
-  const request = () => {
-    
-    const options = {
-      method: "GET",
-      url: "https://random-facts2.p.rapidapi.com/getfact",
-      headers: {
-        "X-RapidAPI-Host": "random-facts2.p.rapidapi.com",
-        "X-RapidAPI-Key": `${process.env.REACT_APP_API_KEY}`,
-      },
-    };
-
-    axios
-      .request(options)
-      .then(function (response) {
-        result = response.data.Fact;
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
-    return result;
-  };
   
   return (
     <div className="app">
-      <Form request={request} />
+      <Form />
     </div>
   );
 }
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,13 +1,39 @@
-import React, {useState} from "react";
-import './Form.scss'
+import React, { useState } from "react";
+import "./Form.scss";
 
-export default function Form ({request}) {
-  const [quote, setQuote] = useState("Hello world")
+export default function Form() {
+  const axios = require("axios");
+  const [quote, setQuote] = useState("Bees sometimes sting other bees.");
+  console.log("QUOTE", quote);
+  
+  const request = () => {
+    
+    const options = {
+      method: "GET",
+      url: "https://random-facts2.p.rapidapi.com/getfact",
+      headers: {
+        "X-RapidAPI-Host": "random-facts2.p.rapidapi.com",
+        "X-RapidAPI-Key": `${process.env.REACT_APP_API_KEY}`,
+      },
+    };
+
+    axios
+      .request(options)
+      .then(function (response) {
+        let result = response.data.Fact;
+        setQuote(result)
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
 
   return (
     <div className="form">
       <div>{quote}</div>
-      <button className="form-button" onClick={() => setQuote(request)}>Next</button>
+      <button className="form-button" onClick={() => setQuote(request())}>
+        Next
+      </button>
     </div>
-  )
+  );
 }
